a84bf7c6890a4cb0f9243b876b0ba353382d1c94
pg1985 <patrick@pushup.com>
2016-02-15 10:15:23 -0600

    adding first commit

---
+++ socket.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int main(int argc, char * argv[])
+{
+	int sockfd, newsockfd, portno, clilen;
+	char buffer[256];
+	struct sockaddress_in, server_address, client_address;
+	int result; //replaces 'n' in code, jackass C programmers
+}
